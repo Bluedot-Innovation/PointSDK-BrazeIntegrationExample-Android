ca1829885e659c44a7d7170a1612f1e7d7cf6422
Nataliia Klymenko <Natalia@bluedot.io>
2024-03-13 14:21:44 +1100

    BD-5431 Update BluePoint SDK to v16.0.0

--- app/src/main/java/bluedot/com/au/bluedotbrazeintegrationapp/BluedotGeoTriggerReceiver.kt
+++ app/src/main/java/bluedot/com/au/bluedotbrazeintegrationapp/BluedotGeoTriggerReceiver.kt
@@ -2,9 +2,8 @@ package bluedot.com.au.bluedotbrazeintegrationapp
 
 import android.content.Context
 import au.com.bluedot.point.net.engine.GeoTriggeringEventReceiver
-import au.com.bluedot.point.net.engine.ZoneEntryEvent
-import au.com.bluedot.point.net.engine.ZoneExitEvent
 import au.com.bluedot.point.net.engine.ZoneInfo
+import au.com.bluedot.point.net.engine.event.GeoTriggerEvent
 import com.braze.Braze
 import com.braze.models.outgoing.BrazeProperties
 import java.util.*
@@ -13,42 +12,43 @@ class BluedotGeoTriggerReceiver: GeoTriggeringEventReceiver() {
     private val customEventEntry = "bluedot_entry"
     private val customEventExit = "bluedot_exit"
 
-    override fun onZoneEntryEvent(entryEvent: ZoneEntryEvent, context: Context) {
-        println("Zone ${entryEvent.zoneInfo.zoneName}, fence ${entryEvent.fenceInfo.name} entered at: ${Date()}")
+    override fun onZoneEntryEvent(geoTriggerEvent: GeoTriggerEvent, context: Context) {
+        println("Zone ${geoTriggerEvent.zoneInfo.name}, fence ${geoTriggerEvent.entryEvent()?.fenceName} entered at: ${Date()}")
 
         val eventProperties = BrazeProperties()
-        eventProperties.addProperty("zone_id", entryEvent.zoneInfo.zoneId)
-        eventProperties.addProperty("zone_name", entryEvent.zoneInfo.zoneName)
-        eventProperties.addProperty("latitude", entryEvent.locationInfo.latitude)
-        eventProperties.addProperty("longitude", entryEvent.locationInfo.longitude)
-        eventProperties.addProperty("fence_id", entryEvent.fenceInfo.id)
-        eventProperties.addProperty("fence_name", entryEvent.fenceInfo.name)
+        eventProperties.addProperty("zone_id", geoTriggerEvent.zoneInfo.id)
+        eventProperties.addProperty("zone_name", geoTriggerEvent.zoneInfo.name)
+        eventProperties.addProperty("latitude",
+            geoTriggerEvent.entryEvent()?.locations?.get(0)?.latitude
+        )
+        eventProperties.addProperty("longitude", geoTriggerEvent.entryEvent()?.locations?.get(0)?.longitude)
+        eventProperties.addProperty("fence_id", geoTriggerEvent.entryEvent()?.fenceId)
+        eventProperties.addProperty("fence_name", geoTriggerEvent.entryEvent()?.fenceName)
 
-        entryEvent.zoneInfo.getCustomData()?.forEach { data ->
+        geoTriggerEvent.zoneInfo.customData?.forEach { data ->
             eventProperties.addProperty(data.key, data.value)
         }
 
         Braze.getInstance(context).logCustomEvent(customEventEntry, eventProperties)
     }
 
-    override fun onZoneExitEvent(exitEvent: ZoneExitEvent, context: Context) {
-        println("Zone ${exitEvent.zoneInfo.zoneName}, fence ${exitEvent.fenceInfo.name} exited at: ${Date()}")
+    override fun onZoneExitEvent(geoTriggerEvent: GeoTriggerEvent, context: Context) {
+        println("Zone ${geoTriggerEvent.zoneInfo.name}, fence ${geoTriggerEvent.exitEvent()?.fenceName} exited at: ${Date()}")
 
         val eventProperties = BrazeProperties()
-        eventProperties.addProperty("zone_id", exitEvent.zoneInfo.zoneId)
-        eventProperties.addProperty("zone_name", exitEvent.zoneInfo.zoneName)
-        eventProperties.addProperty("dwellTime", exitEvent.dwellTime)
-        eventProperties.addProperty("fence_id", exitEvent.fenceInfo.id)
-        eventProperties.addProperty("fence_name", exitEvent.fenceInfo.name)
+        eventProperties.addProperty("zone_id", geoTriggerEvent.zoneInfo.id)
+        eventProperties.addProperty("zone_name", geoTriggerEvent.zoneInfo.name)
+        eventProperties.addProperty("dwellTime", geoTriggerEvent.exitEvent()?.dwellTime)
+        eventProperties.addProperty("fence_id", geoTriggerEvent.exitEvent()?.fenceId)
+        eventProperties.addProperty("fence_name", geoTriggerEvent.exitEvent()?.fenceName)
 
-        exitEvent.zoneInfo.getCustomData()?.forEach { data ->
+        geoTriggerEvent.zoneInfo.customData.forEach { data ->
             eventProperties.addProperty(data.key, data.value)
         }
 
         Braze.getInstance(context).logCustomEvent(customEventExit, eventProperties)
     }
 
-    override fun onZoneInfoUpdate(zones: List<ZoneInfo>, context: Context) {
-        println("Zones updated at: ${Date()} \nZoneInfos count: ${zones.count()}")
+    override fun onZoneInfoUpdate(context: Context) {
     }
 }
